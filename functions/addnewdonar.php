3a3dfe0ea50be23cbb59a003f35021ccc428979b
Mohammad Omar Faruq <37800314+audazfaruq@users.noreply.github.com>
2022-03-16 09:58:41 +0600

    Update addnewdonar.php

--- functions/addnewdonar.php
+++ functions/addnewdonar.php
@@ -5,8 +5,10 @@ $email=$_POST["email"];
 $age=$_POST["age"];
 $password=$_POST["password"];
 
-
+//Database Connection //
 $connect = mysqli_connect("localhost","root","","blood_server_new") or die(mysqli_error($connect));
+
+//Database Inset Query //
 $insertQuery="INSERT INTO new_donar(d_name,d_number,d_email,d_age,u_password) VALUES
 ('$name',$number,'$email',$age,'$password')";
 $runQuery=mysqli_query($connect,$insertQuery) or die(mysqli_error($connect));
